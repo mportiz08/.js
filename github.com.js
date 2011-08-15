d4c8c7e8e40852206f8a09668db1e87e1e0158ce
Marcus Ortiz <mportiz08@gmail.com>
2011-08-15 11:30:56 -0700

    added language page links to github nav

--- github.com.js
+++ github.com.js
@@ -1,1 +1,6 @@
+// change background color of notification link
 $('#header a.notifications_count').css({'background': '#E80C02'});
+
+// add links to hot repo listings for ruby and javascript
+$('#header ul.nav').prepend('<li><strong><a href="http://github.com/languages/Javascript" style="color: #0cbfb3;">js</a></strong></li>');
+$('#header ul.nav').prepend('<li><strong><a href="http://github.com/languages/Ruby" style="color: #bd040f;">rb</a></strong></li>');
