98bf6ef8b48b84e8e3498ea117c177cb3c6721d6
Marcus Ortiz <mportiz08@gmail.com>
2012-01-10 15:26:14 -0800

    not ignoring mobile friends anymore

--- facebook.com.js
+++ facebook.com.js
@@ -1,7 +1,7 @@
 // hackish way of removing offline friends from the chat sidebar in facebook
 setTimeout(function() {
   $('ul.fbChatOrderedList li.item').each(function(idx, item) {
-    if(!$(this).hasClass('active')) {
+    if(!($(this).hasClass('active') || $(this).hasClass('mobile'))) {
       $(this).hide();
     }
   });
